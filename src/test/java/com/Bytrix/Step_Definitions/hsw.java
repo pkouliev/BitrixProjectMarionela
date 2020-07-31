55e34d6a54723610498fa35c483d9783750647aa
stefanmitkovic <stefanmitkovic@gmail.com>
2020-07-30 22:24:38 -0400

    ss

--- src/test/java/com/Bytrix/Step_Definitions/hsw.java
+++ src/test/java/com/Bytrix/Step_Definitions/hsw.java
@@ -2,4 +2,12 @@ package com.Bytrix.Step_Definitions;
 
 public class hsw {
     //dont change
+
+    public static void main(String[] args) {
+        System.out.println("sssssssssss");
+
+        System.out.println("wwwwwwwwwwwwwwww");
+
+        System.out.println("wwwwwwwwwwww");
+    }
 }
